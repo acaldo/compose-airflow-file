4daa30824be7fcadb33dd39bee6bd76d270f6e69
caldev <caldart123@gmail.com>
2023-10-23 11:36:10 -0300

    file

---
+++ airflow-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Download the Airflow Docker Compose file
+curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.7.2/docker-compose.yaml'
+
+# Create the necessary directories
+mkdir -p ./dags ./logs ./plugins ./config
+
+# Set the Airflow user ID
+echo -e "AIRFLOW_UID=1000" > .env
